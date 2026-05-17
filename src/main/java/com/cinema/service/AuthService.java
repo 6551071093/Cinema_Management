e9472b58ff2687b468289e24e8808507af90bbcb
thuhuyen-1612 <6551071037@st.utc2.edu.vn>
2026-05-17 23:30:30 +0700

    Update AuthService.java

--- src/main/java/com/cinema/service/AuthService.java
+++ src/main/java/com/cinema/service/AuthService.java
@@ -1,5 +1,29 @@
 package com.cinema.service;
 
-public class AuthService {
+import com.cinema.dao.AccountDAO;
+import com.cinema.entity.Account;
+import org.mindrot.jbcrypt.BCrypt;
 
+public class AuthService {
+    private AccountDAO accountDAO = new AccountDAO();
+    private static Account loggedInAccount;
+
+    public Account login(String username, String password) {
+        Account account = accountDAO.findByUsername(username);
+        if (account != null) {
+            if (BCrypt.checkpw(password, account.getPassword())) {
+                loggedInAccount = account;
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public static Account getLoggedInAccount() {
+        return loggedInAccount;
+    }
+
+    public static void logout() {
+        loggedInAccount = null;
+    }
 }
