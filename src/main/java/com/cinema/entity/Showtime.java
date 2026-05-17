ab916f6f72c8ece5305cea74d8f457386a771d0e
Trần Minh Tú <6551071093@st.utc2.edu.vn>
2026-05-17 20:29:08 +0700

    Update Showtime.java

--- src/main/java/com/cinema/entity/Showtime.java
+++ src/main/java/com/cinema/entity/Showtime.java
@@ -1,5 +1,41 @@
 package com.cinema.entity;
 
-public class Showtime {
+import java.sql.Timestamp;
 
+public class Showtime {
+    private int id;
+    private int movieId;    
+    private int roomId;     
+    private Timestamp startTime;
+    private Timestamp endTime;
+    private double ticketPrice;
+
+    public Showtime() {}
+
+    public Showtime(int id, int movieId, int roomId, Timestamp startTime, Timestamp endTime, double ticketPrice) {
+        this.id = id;
+        this.movieId = movieId;
+        this.roomId = roomId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.ticketPrice = ticketPrice;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getMovieId() { return movieId; }
+    public void setMovieId(int movieId) { this.movieId = movieId; }
+
+    public int getRoomId() { return roomId; }
+    public void setRoomId(int roomId) { this.roomId = roomId; }
+
+    public Timestamp getStartTime() { return startTime; }
+    public void setStartTime(Timestamp startTime) { this.startTime = startTime; }
+
+    public Timestamp getEndTime() { return endTime; }
+    public void setEndTime(Timestamp endTime) { this.endTime = endTime; }
+
+    public double getTicketPrice() { return ticketPrice; }
+    public void setTicketPrice(double ticketPrice) { this.ticketPrice = ticketPrice; }
 }
