7e91db593de36faa4038678f8d6b3d2a32d8a004
quocquyhoccode <dangquocquy2020@gmail.com>
2026-05-17 20:42:28 +0700

    Implement DatabaseConnection class with connection methods

--- src/main/java/com/cinema/dao/DatabaseConnection.java
+++ src/main/java/com/cinema/dao/DatabaseConnection.java
@@ -1,5 +1,41 @@
 package com.cinema.dao;
 
-public class DatabaseConnection {
+import com.cinema.utils.ConfigReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnection {
+    
+    private static final String URL = ConfigReader.getProperty("db.url");
+    private static final String USER = ConfigReader.getProperty("db.username");
+    private static final String PASSWORD = ConfigReader.getProperty("db.password");
+    
+    private static Connection connection = null;
+
+    private DatabaseConnection() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Ket noi Database CinemaManagement thanh cong!");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
