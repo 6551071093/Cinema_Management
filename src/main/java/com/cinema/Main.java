4b2f929f3da0b46444484484e65729a64c25d96d
quocquyhoccode <dangquocquy2020@gmail.com>
2026-05-17 20:40:28 +0700

    Initialize FlatLaf and launch LoginFrame

--- src/main/java/com/cinema/Main.java
+++ src/main/java/com/cinema/Main.java
@@ -1,10 +1,25 @@
 package com.cinema;
 
+import com.cinema.ui.LoginFrame;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Không thể khởi tạo giao diện FlatLaf. Đang dùng mặc định.");
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        SwingUtilities.invokeLater(() -> {
+            try {
+                LoginFrame loginFrame = new LoginFrame();
+                loginFrame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
