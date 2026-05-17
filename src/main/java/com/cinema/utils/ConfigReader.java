586dccdbc26360d582a61fd5afb23ff302ba7b78
quocquyhoccode <dangquocquy2020@gmail.com>
2026-05-17 21:13:57 +0700

    Implement static block to load configuration properties
    Load properties from database.properties file during static initialization.

--- src/main/java/com/cinema/utils/ConfigReader.java
+++ src/main/java/com/cinema/utils/ConfigReader.java
@@ -1,5 +1,24 @@
 package com.cinema.utils;
 
-public class ConfigReader {
+import java.io.InputStream;
+import java.util.Properties;
 
+public class ConfigReader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("database.properties")) {
+            if (input == null) {
+                System.err.println("Không tìm thấy file database.properties trong thư mục resources.");
+            } else {
+                properties.load(input);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
